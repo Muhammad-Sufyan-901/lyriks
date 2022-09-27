dc03f6084f511f7b4dd33cff26a02837c5d53270
zetta-YanChris-Difa <chris.difa@zettabyte.life>
2022-09-27 21:02:57 +0800

    Inital Commit

---
+++ src/pages/SongDetails.jsx
@@ -0,0 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetRelatedSongsQuery, useGetSongDetailsQuery } from "../redux/services/shazamCore";
+
+const SongDetails = () => {
+  const { songid } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+  const { data, isFetching: isFetchingRelatedSongs, error } = useGetRelatedSongsQuery({ songid });
+  const dispatch = useDispatch();
+
+  const handlePauseOnClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayOnClick = (song, index) => {
+    dispatch(setActiveSong({ song, data, index }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingSongDetails || isFetchingRelatedSongs) return <Loader title="Searching Song Details" />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistsId="" songData={songData} />
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics :</h2>
+
+        <div className="mt-5">
+          {songData?.sections[1].type === "LYRICS" ? (
+            songData?.sections[1]?.text.map((lyrics, index) => (
+              <p key={index} className="text-gray-400 text-base my-1">
+                {lyrics}
+              </p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">Sorry, No Lyrics Found!</p>
+          )}
+        </div>
+      </div>
+
+      <RelatedSongs data={data} isPlaying={isPlaying} activeSong={activeSong} handlePlay={handlePlayOnClick} handlePause={handlePauseOnClick} />
+    </div>
+  );
+};
+
+export default SongDetails;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", "d95659134cmsh63afc38a8e80880p17e6a4jsnf6f6ab7ff273");
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getRelatedSongs: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetRelatedSongsQuery } = shazamCoreApi;
